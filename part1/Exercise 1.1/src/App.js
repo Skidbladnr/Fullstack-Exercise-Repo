c6b461619a0da49ece81732c9cbde9a09da5b09d
Skidbladnr <86094336+Skidbladnr@users.noreply.github.com>
2022-09-13 16:16:17 -0400

    Add files via upload

---
+++ part1/Exercise 1.1/src/App.js
@@ -0,0 +1,48 @@
+const Header = (props) => {
+  return (
+  <div>
+    <h1>{props.hcourse}</h1>
+  </div> 
+  )
+}
+const Content = (props) => {
+  return (
+  <div>
+    <p>
+      {props.part1} {props.exercises1}
+    </p>
+    <p>
+      {props.part2} {props.exercises2}
+    </p>
+    <p>
+      {props.part3} {props.exercises3}
+    </p>
+  </div>
+  )
+}
+const Total = (props) => {
+  return(
+  <div>
+    <p>Number of exercises {props.e1 + props.e2 + props.e3}</p>
+  </div>
+  )
+}
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header hcourse={course}/>
+      <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3}/>
+      <Total e1={exercises1} e2={exercises2} e3={exercises3} />      
+    </div>
+  )
+}
+
+export default App
