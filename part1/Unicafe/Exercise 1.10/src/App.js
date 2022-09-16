285c8e0dd2049f91e52ceeec1322c70ecba2d4b1
Skidbladnr <86094336+Skidbladnr@users.noreply.github.com>
2022-09-16 17:33:02 -0400

    Add files via upload

---
+++ part1/Unicafe/Exercise 1.10/src/App.js
@@ -0,0 +1,79 @@
+import { useState } from 'react'
+var counter = 0
+
+const StatisticLine = (props) => {
+  var voteNum = props.value[0] + props.value[1] + props.value[2] 
+  var totalScore = props.value[0] + (props.value[2] * - 1)
+  var sendValue = 0
+  if(voteNum == 0 && counter == 0){
+    counter += 1
+    return (
+      <div>No feedback given.</div>
+    )
+  }
+  else if(voteNum != 0){
+    if(props.text.valueOf() == "average"){
+      sendValue = totalScore/voteNum
+    }
+    else if(props.text.valueOf() =="positive"){
+      sendValue = (props.value[0]/voteNum) + " %"
+    }
+    else if(props.text.valueOf() =="all"){
+      sendValue = voteNum
+    }
+    else if(props.text.valueOf() =="good"){
+      sendValue = props.value[0]
+    }
+    else if(props.text.valueOf() =="neutral"){
+      sendValue = props.value[1]
+    }
+    else if(props.text.valueOf() =="bad"){
+      sendValue = props.value[2]
+    }
+    return (
+      <div>{props.text} {sendValue}</div>
+    )
+  }  
+}
+
+const Statistics = (props) => {
+  return (
+    <div>
+      <h1>statistics</h1>
+      <div>
+        <StatisticLine text="good" value={[props.good, props.neutral, props.bad]}/>
+        <StatisticLine text="neutral" value={[props.good, props.neutral, props.bad]}/>
+        <StatisticLine text="bad" value={[props.good, props.neutral, props.bad]}/>
+        <StatisticLine text="all" value={[props.good, props.neutral, props.bad]}/>
+        <StatisticLine text="average" value={[props.good, props.neutral, props.bad]}/>
+        <StatisticLine text="positive" value={[props.good, props.neutral, props.bad]}/>
+      </div>
+
+    </div>
+  )   
+}
+  
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <button onClick={() => setGood(good+1)}>
+      good
+      </button>  
+      <button onClick={() => setNeutral(neutral+1)}>
+      neutral
+      </button>  
+      <button onClick={() => setBad(bad+1)}>
+      bad
+      </button>  
+      <Statistics good={good} neutral={neutral} bad={bad}/> 
+    </div>
+  )
+}
+
+export default App
